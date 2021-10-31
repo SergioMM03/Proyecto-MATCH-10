4c15ebb4a6118e76a0b16dadcc9cf184cda749fd
Sergio <smandujano106@gmail.com>
2021-10-31 10:52:12 -0600

    Primera etapa

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+void Mmatriz(int dim,int matriz[dim][dim]);
+void llenarMatriz(int dim,int matriz[dim][dim]);
+
+int main(){
+    srand(time(NULL));
+    int a;
+    int dim = 9;
+    int matriz[dim][dim];
+    llenarMatriz(dim,matriz);
+    Mmatriz(dim,matriz);
+      
+    return 0;
+
+}
+
+void Mmatriz(int dim, int matriz[dim][dim]){
+    int i,j;
+    for (i=0; i<9; i++){
+        for (j=0; j<9; j++){
+            printf("%d\t", matriz[i][j]);
+        }
+         printf("\n\n\n");
+    }
+}
+
+void llenarMatriz( int dim, int matriz[dim][dim]){
+    int i,j;
+    for (i=0; i<dim; i++){
+        for (j=0; j<dim; j++){
+            matriz[i][j]=1+rand() % 9;
+        }
+    }
+}
